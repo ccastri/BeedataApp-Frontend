2827168cb9aaace424b2a76232aa0873161c3ecc
Canel420 <4521@holbertonstudents.com>
2023-04-10 15:21:22 -0500

    Webpack config added

--- next.config.js
+++ next.config.js
@@ -1,23 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Add custom Babel configuration to ignore license files
+    // Exclude LICENSE files from being processed
     config.module.rules.push({
-      test: /\.(js|mjs)$/,
-      include: /node_modules/,
-      exclude: /@babel(?:\/|\\{1,2})plugin-/,
-      use: [
-        defaultLoaders.babel,
-        {
-          loader: 'ignore-loader',
-          options: {
-            ignore: [
-              // Add the license files that you want to ignore here
-              /\.\/LICENSE$/,
-            ],
-          },
-        },
-      ],
+      test: /\.LICENSE$/,
+      use: 'null-loader',
     });
 
     return config;
