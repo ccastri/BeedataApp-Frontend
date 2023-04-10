7b3bf6ac5c301bf9676c888fb2f0ea04383cb308
Canel420 <4521@holbertonstudents.com>
2023-04-10 14:47:34 -0500

    Webpack config added

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(md|LICENSE)$/, // Add a rule to handle .md and LICENSE files
+      use: 'raw-loader' // Use raw-loader to load these file types as raw text
+    });
+
+    return config;
+  }
 };
