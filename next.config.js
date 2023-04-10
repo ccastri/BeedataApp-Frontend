c513f432ba24eb3e15127e6da48c07d3b07af599
Canel420 <4521@holbertonstudents.com>
2023-04-10 15:02:16 -0500

    Webpack config added

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 module.exports = {
   reactStrictMode: true,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Ignore README.md file for @babel/plugin-syntax-bigint
+    // Add custom Babel configuration to ignore license files
     config.module.rules.push({
-      test: /@babel\/plugin-syntax-bigint/,
-      use: 'null-loader',
+      test: /\.(js|mjs)$/,
+      include: /node_modules/,
+      exclude: /@babel(?:\/|\\{1,2})plugin-/,
+      use: [
+        defaultLoaders.babel,
+        {
+          loader: 'ignore-loader',
+          options: {
+            ignore: [
+              // Add the license files that you want to ignore here
+              /\.\/LICENSE$/,
+            ],
+          },
+        },
+      ],
     });
 
     return config;
