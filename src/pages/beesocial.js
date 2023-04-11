8feed438cc37fa1b992fb844ae2d1a125ac5162a
Canel420 <4521@holbertonstudents.com>
2023-04-11 13:25:19 -0500

    Beesocial removed temoporarily

--- src/pages/beesocial.js
+++ src/pages/beesocial.js
@@ -2,16 +2,7 @@ import Head from 'next/head';
 import { RocketchatLayout } from '../components/rocketchat/rocket-layout';
 
 const Page = ({ rocketChatUrl }) => {
-  const handleGoToGeneral = () => {
-    document.querySelector("iframe").contentWindow.postMessage(
-      {
-        externalCommand: "go",
-        path: "/home"
-      },
-      rocketChatUrl
-    );
-  };
-
+  
   return (
     <>
       <Head>
@@ -21,28 +12,10 @@ const Page = ({ rocketChatUrl }) => {
         <title>Beesocial | Beedata</title>
       </Head>
       <RocketchatLayout>
-        <iframe
-          src={`${rocketChatUrl}/home`}
-          title="Beesocial"
-          style={{
-            width: '100%',
-            height: '100%',
-            position: 'absolute',
-            marginLeft: 0,
-            marginTop: 0,
-            border: 'none'
-          }}
-        />
+
       </RocketchatLayout>
     </>
   );
-};
-
-export const getStaticProps = async () => {
-  const rocketChatUrl = process.env.BEESOCIAL_SERVER;
-  return {
-    props: { rocketChatUrl }
-  }
 };
 
 export default Page;
