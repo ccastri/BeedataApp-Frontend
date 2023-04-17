5114ae11eddf42a5d4e4ff42ffa282f428d76cab
Canel420 <4521@holbertonstudents.com>
2023-04-17 15:37:13 -0500

    Export path

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-source /home/ubuntu/.bashrc
+
+export PATH=$PATH:/usr/bin:/usr/local/bin:/usr/local/lib/node_modules/pm2/bin >> /home/ubuntu/deploy.log
+
 echo 'run application_start.sh: ' >> /home/ubuntu/deploy.log
 cd /home/ubuntu/frontend
 
-echo 'pm2 restart frontend' >> /home/ubuntu/deploy.log
+echo 'pm2 start frontend' >> /home/ubuntu/deploy.log
 pm2 start npm --name "frontend" -- start >> /home/ubuntu/deploy.log
