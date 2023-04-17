4e0b7dcc628c0f5caf3f5d27a7c8922f161d1b11
Canel420 <4521@holbertonstudents.com>
2023-04-17 15:55:40 -0500

    overwrite build

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,4 +5,4 @@ echo 'run application_start.sh: ' >> /home/ubuntu/deploy.log
 cd /home/ubuntu/frontend
 
 echo 'pm2 start frontend' >> /home/ubuntu/deploy.log
-pm2 start npm --name "frontend" -- start >> /home/ubuntu/deploy.log
+pm2 restart frontend >> /home/ubuntu/deploy.log
