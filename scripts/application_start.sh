2ffb493833be82250809dc40c74ae20987615ce2
Canel420 <4521@holbertonstudents.com>
2023-04-17 15:03:55 -0500

    Add before install command

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,4 +4,4 @@ echo 'run application_start.sh: ' >> /home/ubuntu/deploy.log
 cd /home/ubuntu/frontend
 
 echo 'pm2 restart frontend' >> /home/ubuntu/deploy.log
-pm2 restart frontend >> /home/ubuntu/deploy.log
+pm2 start npm --name "frontend" -- start >> /home/ubuntu/deploy.log
