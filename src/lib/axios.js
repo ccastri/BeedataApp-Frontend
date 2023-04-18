7810eca4fc3cdd0be90aabd25bc4addd0cbed091
Canel420 <4521@holbertonstudents.com>
2023-04-18 10:28:05 -0500

    Checking env variables

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const backendServer = process.env.NODE_ENV === 'production' ? process.env.BACKEND_SERVER : 'http://localhost:3001'
 
 const api = axios.create({
-    baseURL: 'https://api.beet.digital',
+    baseURL: backendServer,
 });
 
 export default api;
