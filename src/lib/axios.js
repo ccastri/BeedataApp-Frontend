98c925d9bd8c377d8ed4d3f6690c9abace99ec1c
Canel420 <4521@holbertonstudents.com>
2023-04-18 07:13:17 -0500

    Fixed typo

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 const backendServer = process.env.NODE_ENV === 'production' ? process.env.BACKEND_SERVER : 'http://localhost:3001'
 
 const api = axios.create({
-    baseURL: backendServer,
+    baseURL: 'https://api.beet.digital',
 });
