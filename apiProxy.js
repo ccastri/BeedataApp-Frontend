963ca05e48d3d7752a4035d30ba15b43bc013845
Canel420 <4521@holbertonstudents.com>
2023-04-18 05:36:43 -0500

    Changed proxy method 2

--- apiProxy.js
+++
@@ -1,8 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-const apiProxy = createProxyMiddleware('/api', {
-  target: 'https://api.beet.digital',
-  changeOrigin: true,
-});
-
-module.exports = { apiProxy };
--- next.config.js
+++ next.config.js
@@ -2,15 +2,4 @@ const { apiProxy } = require('./apiProxy');
 
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      { source: '/api/:path*', destination: '/api/:path*' },
-      { source: '/api', destination: '/api/' },
-    ];
-  },
-  async serverMiddleware() {
-    return [
-      apiProxy,
-    ];
-  },
 };
