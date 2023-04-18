dbcc4df3000481528287201f93d732d09c01a017
Canel420 <4521@holbertonstudents.com>
2023-04-18 06:22:44 -0500

    Changed proxy method 7

--- src/pages/api/[...slug].js
+++ src/pages/api/[...slug].js
@@ -1,19 +1,24 @@
-import { createProxyMiddleware } from "http-proxy-middleware"; // @2.0.6
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const proxy = createProxyMiddleware({
-  target: process.env.BACKEND_SERVER,
-  secure: false,
-  pathRewrite: { "^/api/proxy": "" },
+  // Replace with your target URL here
+  target: 'https://api.beet.digital',
+  changeOrigin: true,
+  // Prevents some security issues
+  cookieDomainRewrite: '',
+  onProxyReq: (proxyReq) => {
+    // Remove any `X-Forwarded-*` headers sent by the client
+    Object.keys(proxyReq.headers)
+      .filter((key) => key.startsWith('x-forwarded'))
+      .forEach((key) => delete proxyReq.headers[key]);
+    // Add or modify headers as necessary
+    proxyReq.setHeader('User-Agent', 'proxy-agent');
+  },
 });
 
 export default function handler(req, res) {
-  proxy(req, res, (err) => {
-    if (err) {
-      throw err;
-    }
-
-    throw new Error(
-      `Request '${req.url}' is not proxied! We should never reach here!`
-    );
-  });
+  // Note: This assumes your Next.js app is running on the root path '/'
+  const [_, ...slug] = req.query.slug || [];
+  const targetUrl = `/${slug.join('/')}`;
+  return proxy(req, res, { target: targetUrl });
 }
