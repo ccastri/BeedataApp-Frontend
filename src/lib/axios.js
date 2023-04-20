94cf2a66b63a0186e9e74582a9aca817a82d9bf9
Canel420 <4521@holbertonstudents.com>
2023-04-20 10:40:46 -0500

    Enviromental variable check

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const backendServer = process.env.BACKEND_SERVER;
+const backendServer = 'https://api.beet.digital'
 
 const api = axios.create({
     baseURL: backendServer,
