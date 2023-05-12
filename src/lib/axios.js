f87c4be65f1c0901c804723686c96f05e82e0abf
Canel420 <4521@holbertonstudents.com>
2023-05-12 16:17:45 -0500

    Env variables configuration try:6

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-// import getConfig from 'next/config';
+import getConfig from 'next/config';
 
-// const { publicRuntimeConfig } = getConfig();
-// const backendServer = publicRuntimeConfig.BACKEND_URL;
+const { publicRuntimeConfig } = getConfig();
+const backendServer = publicRuntimeConfig.BACKEND_URL;
 
 const api = axios.create({
-    baseURL: 'https://api.beet.digital',
+    baseURL: backendServer,
 });
 
 export default api;
